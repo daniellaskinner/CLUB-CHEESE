c8c7adb69485bd07e4c15238538e7d60e6c62452
Nelly <43520843+daniellaskinner@users.noreply.github.com>
2019-05-12 14:39:56 +0100

    Added existing files from computer for project

---
+++ jspractice.js
@@ -0,0 +1,16 @@
+alert("Welcome to Club Cheese!");
+
+var age= prompt("Please enter your age")
+
+if (age<18) {
+  alert("Sorry, you are not old enough to enter the venue.")
+}
+
+else if (age<21) {
+  alert("You may enter the club, but you won't be allowed any cheesy cocktails!")
+
+}
+
+else {
+  alert("Come on in and join us for a drink!")
+}
